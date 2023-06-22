34b5e997b4b852d26207c1bc08daa8eecfd4d7d4
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-06-22 11:36:07 +0200

    add plotting for surveillance on/off

--- security-camera/src/stats_data_visualizer.py
+++ security-camera/src/stats_data_visualizer.py
@@ -49,8 +49,48 @@ class StatsDataVisualizer:
         plt.xlabel('datetime')
         pyplot.show()
 
-    def show_surveillance_plot(self):
-        pass
+    def transform_surveillance_data(self, date_from, date_to):
+        """Filters surveillance data and prepares it for plotting.
+            :param date_from starting date in milliseconds
+            :param date_to ending date in milliseconds
+            :return: x, y ready for plotting"""
+
+        surveillance_data = self.__stats_data_manager.fetch_surveillance_log()
+        surveillance_data.sort(key=lambda log: log[0])
+        surveillance_data_between_dates = []
+
+        i = 0
+        while i < len(surveillance_data) and surveillance_data[i][0] < date_from:
+            i += 1
+        while i < len(surveillance_data) and surveillance_data[i][0] <= date_to:
+            dt = datetime.fromtimestamp(surveillance_data[i][0] / 1000)
+
+            if surveillance_data[i][1] == 'ON':
+                surveillance_data_between_dates.append((str(dt)[5:16], 'OFF'))
+            else:
+                surveillance_data_between_dates.append((str(dt)[5:16], 'ON'))
+
+            surveillance_data_between_dates.append((str(dt)[5:16], surveillance_data[i][1]))
+            i += 1
+
+        return [log[0] for log in surveillance_data_between_dates],\
+            [1 if log[1] == 'ON' else 0 for log in surveillance_data_between_dates]
+
+    def show_surveillance_plot(self, date_from, date_to):
+        """Plots surveillance data between provided dates.
+            :param date_from starting date in milliseconds
+            :param date_to ending date in milliseconds
+            :return: None"""
+
+        xs, ys = self.transform_surveillance_data(date_from, date_to)
+        fig, ax = pyplot.subplots()
+        pyplot.gcf().autofmt_xdate()
+        ax.xaxis.set_major_locator(ticker.MaxNLocator(12))
+        pyplot.plot(xs, ys)
+        plt.yticks([])
+        plt.ylabel('surveillance on')
+        plt.xlabel('datetime')
+        pyplot.show()
 
     def show_notification_plot(self):
         pass

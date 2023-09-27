df4e93c21d5cfb74b6701b307869db7f3ab81656
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-09-27 16:18:22 +0200

    Add more Camera tests

--- security-camera/tests/conftest.py
+++ security-camera/tests/conftest.py
@@ -6,14 +6,19 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.camera import Camera
 
 
-@pytest.fixture(scope="module", params=(
+@pytest.fixture(scope="function", params=(
     {"emergency_buff_size": 20, "detection_sensitivity": 1, "max_detection_sensitivity": 25, "min_motion_contour_area": 10, "fps": 100, "camera_number": 0, "recording_mode": "Gray"},
+    
+    {"emergency_buff_size": 200, "detection_sensitivity": 10, "max_detection_sensitivity": 15, "min_motion_contour_area": 1000, "fps": 24, "camera_number": 0, "recording_mode": "Sharpened"}
 ))
 def camera_params(request):
     yield request.param
 
 
-@pytest.fixture(name="camera", scope="module")
+@pytest.fixture(name="camera", scope="function")
 def make_camera(camera_params):
-    return Camera(camera_params["emergency_buff_size"], camera_params["detection_sensitivity"], camera_params     ["max_detection_sensitivity"],camera_params["min_motion_contour_area"], camera_params["fps"], camera_params["camera_number"], camera_params["recording_mode"])
-
+    camera = Camera(camera_params["emergency_buff_size"], camera_params["detection_sensitivity"], camera_params     ["max_detection_sensitivity"],camera_params["min_motion_contour_area"], camera_params["fps"], camera_params["camera_number"], camera_params["recording_mode"])
+    
+    yield camera
+    
+    camera.destroy()
--- security-camera/tests/test_camera.py
+++ security-camera/tests/test_camera.py
@@ -1,22 +1,41 @@
+from src.camera import Camera
 import pytest
 import os
 import sys
+import numpy as np
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from src.camera import Camera
 
 
-@pytest.mark.usefixtures("camera") 
+@pytest.mark.usefixtures("camera")
 def test_capture(camera: Camera):
     assert camera.validate_capture()
 
 
-@pytest.mark.usefixtures("camera") 
+@pytest.mark.usefixtures("camera")
 def test_destroy(camera: Camera):
     assert camera.validate_capture()
-    
+
     camera.destroy()
-    
+
     assert not camera.validate_capture()
     assert not camera.emergency_recording_started
     assert not camera.standard_recording_started
+
+
+@pytest.mark.usefixtures("camera")
+def test_refresh_frame_no_capture(camera: Camera):
+    assert camera.validate_capture()
+    
+    camera.destroy()
+
+    assert not camera.validate_capture()
+
+
+@pytest.mark.usefixtures("camera")
+def test_validate_frame(camera: Camera):
+    assert not camera.validate_frame(None)
+    assert not camera.validate_frame("None")
+    
+    for resolution in ((1, 1), (1280, 720), (1920, 1080), (2560, 1440)):
+        assert camera.validate_frame(np.random.choice(range(256), resolution))
